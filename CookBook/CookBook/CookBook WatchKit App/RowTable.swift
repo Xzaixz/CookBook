8caf6ac7edf7e24d783b58460693a6351326703e
Xzaixz <75606948+Xzaixz@users.noreply.github.com>
2022-02-14 17:38:20 +0300

    Add files via upload

---
+++ CookBook/CookBook/CookBook WatchKit App/RowTable.swift
@@ -0,0 +1,15 @@
+//
+//  RowTable.swift
+//  CookBook
+//
+//  Created by Михаил  Галицкий on 14.02.2022.
+//
+
+import WatchKit
+
+class RowTable: NSObject {
+    
+    @IBOutlet weak var rowPicture: WKInterfaceImage!
+    @IBOutlet weak var rowRecipeName: WKInterfaceLabel!
+
+}
